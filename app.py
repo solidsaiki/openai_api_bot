d959c39950f53488b9539db636356617d854fef7
solidsaiki <132334871+solidsaiki@users.noreply.github.com>
2023-05-02 16:31:29 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def communicate():
 
 # ユーザーインターフェイスの構築
 st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.write("ChatGPT APIを使った新しいチャットボットです。")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
